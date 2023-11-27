bf17299b0267ae4cddd92e184c74ef17e2703270
NieHong <nie2669780@163.com>
2023-11-27 23:18:40 +0800

    fix(reference): 引用类型生命周期打印

--- _02_jvm-hotspot/src/main/java/online/niehong/reference/ReferenceTypeDemo.java
+++ _02_jvm-hotspot/src/main/java/online/niehong/reference/ReferenceTypeDemo.java
@@ -19,6 +19,12 @@ public class ReferenceTypeDemo {
         WeakReference<String> integerWeakReference = new WeakReference<>(new String("我是弱引用对象,快运行看看我还在不在了"));
         PhantomReference<String> integerPhantomReference = new PhantomReference<>(new String("我是虚引用对象,快运行看看我还在不在了"), new ReferenceQueue<>());
 
+        System.out.println("强引用：" + object);
+        System.out.println("软引用：" + integerSoftReference.get());
+        System.out.println("弱引用：" + integerWeakReference.get());
+        System.out.println("虚引用：" + integerPhantomReference.get());
+
+        System.out.println("\n\n\n手动垃圾回收后");
         System.gc();
         System.out.println("强引用：" + object);
         System.out.println("软引用：" + integerSoftReference.get());
