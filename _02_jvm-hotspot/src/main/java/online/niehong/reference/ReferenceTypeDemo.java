7f9d9d04ac512b880692fb1bb3bd82d80207d5f7
NieHong <nie2669780@163.com>
2023-11-27 17:40:00 +0800

    fix(reference): 移除未使用的import

--- _02_jvm-hotspot/src/main/java/online/niehong/reference/ReferenceTypeDemo.java
+++ _02_jvm-hotspot/src/main/java/online/niehong/reference/ReferenceTypeDemo.java
@@ -1,14 +1,10 @@
 package online.niehong.reference;
 
 
-
 import java.lang.ref.PhantomReference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 引用类型演示
