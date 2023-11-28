5fbca999f723252fd4fadc056e151d4d9b05da0b
NieHong <nie2669780@163.com>
2023-11-28 16:24:53 +0800

    fix(JUC): 并发工具类
    Condition使用示例

--- _03_concurrent-programming/src/main/java/online/niehong/juc/collaborative/Tools.java
+++ _03_concurrent-programming/src/main/java/online/niehong/juc/collaborative/Tools.java
@@ -1,6 +1,9 @@
 package online.niehong.juc.collaborative;
 
 import java.util.concurrent.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * JUC的线程协作工具类
@@ -10,9 +13,10 @@ import java.util.concurrent.*;
  */
 public class Tools {
     public static void main(String[] args) {
-        countDownLatch(new CountDownLatch(5));
-        semaphore(new Semaphore(2), 5);
-        cyclicBarrier(new CyclicBarrier(3, () -> System.out.println("存入银行")));
+//        countDownLatch(new CountDownLatch(5));
+//        semaphore(new Semaphore(2), 5);
+//        cyclicBarrier(new CyclicBarrier(3, () -> System.out.println("存入银行")));
+        condition();
     }
 
     /**
@@ -70,7 +74,7 @@ public class Tools {
 
     /**
      * 循环栅栏
-     *  - 模拟存钱，存到多少钱了就存到银行里去
+     * - 模拟存钱，存到多少钱了就存到银行里去
      *
      * @param cyclicBarrier 循环栅栏
      */
@@ -86,6 +90,103 @@ public class Tools {
                     throw new RuntimeException(e);
                 }
             }).start();
+        }
+    }
+
+    /**
+     * 条件
+     * - 模拟五个人一天的作息顺序
+     */
+    private static void condition() {
+        Work work = new Work();
+
+        // 起床
+        for (int i = 0; i < 5; i++) {
+            new Thread(work::getUp).start();
+        }
+
+        // 去工作
+        for (int i = 0; i < 5; i++) {
+            new Thread(work::gotoWork).start();
+        }
+
+        // 下班了
+        for (int i = 0; i < 5; i++) {
+            new Thread(work::goHome).start();
+        }
+
+    }
+
+    /**
+     * 工作
+     *
+     * @author NieHong
+     * @date 2023/11/28
+     */
+    static class Work {
+        private volatile int step = 1;
+        Lock lock = new ReentrantLock();
+        private final Condition c1 = lock.newCondition();
+        private final Condition c2 = lock.newCondition();
+        private final Condition c3 = lock.newCondition();
+
+        public Work() {
+        }
+
+        /**
+         * 起床
+         */
+        public void getUp() {
+            lock.lock();
+            try {
+                while (step != 1) {
+                    c1.await();
+                }
+                System.out.println("起床");
+                step = 2;
+                c2.signal();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        /**
+         * 去工作
+         */
+        public void gotoWork() {
+            lock.lock();
+            try {
+                while (step != 2) {
+                    c2.await();
+                }
+
+                System.out.println("去工作");
+                step = 3;
+                c3.signal();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        public void goHome() {
+            lock.lock();
+
+            try {
+                while (step != 3) {
+                    c3.await();
+                }
+                System.out.println("下班回家了");
+                step = 1;
+                c1.signal();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
         }
     }
 }
