bdee93651ccb06e2e9848143e090d35870882dd7
NieHong <nie2669780@163.com>
2023-11-28 13:38:08 +0800

    fix(JUC): 并发工具类
    信号量的使用示例

--- _03_concurrent-programming/src/main/java/online/niehong/juc/collaborative/Tools.java
+++ _03_concurrent-programming/src/main/java/online/niehong/juc/collaborative/Tools.java
@@ -1,6 +1,7 @@
 package online.niehong.juc.collaborative;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class Tools {
     public static void main(String[] args) {
         countDownLatch(new CountDownLatch(5));
+        semaphore(new Semaphore(2), 5);
     }
 
     /**
@@ -21,7 +23,6 @@ public class Tools {
      * @param downLatch 计数门闩
      */
     private static void countDownLatch(CountDownLatch downLatch) {
-
         for (long i = 0; i < downLatch.getCount(); i++) {
             new Thread(() -> {
                 try {
@@ -42,5 +43,30 @@ public class Tools {
             }
             System.out.println(Thread.currentThread().getName() + "最后下班，无敌的存在");
         }, "boss").start();
+    }
+
+    /**
+     * 信号量
+     * 信号量的使用
+     * - 模拟停车场车位的抢占情况，即指定的车位，只能停指定的车，多余的车则处于等待状态
+     *
+     * @param semaphore 信号量(具体能停多少俩车)
+     * @param carNumber 车的数量
+     */
+    private static void semaphore(Semaphore semaphore, int carNumber) {
+        for (int i = 0; i < carNumber; i++) {
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "抢到了车位");
+                    TimeUnit.MILLISECONDS.sleep(300);
+                    System.out.println(Thread.currentThread().getName() + "离开了车位");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    semaphore.release();
+                }
+            }, "汽车" + i + " ").start();
+        }
     }
 }
